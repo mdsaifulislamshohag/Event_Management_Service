6ef356b797834e41c22ab3a719f50896bd56b1eb
saifuldev0 <mdsaifulislamshohag79@gmail.com>
2022-11-16 14:53:11 +0600

    setting up mysql connection

---
+++ config/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+const pool = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+module.exports = pool.promise();
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const config = require('dotenv').config({ path: ".env" })
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
