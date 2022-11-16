5c14c891a8e10c25307978092884bcb8e08bcbc3
saifuldev0 <mdsaifulislamshohag79@gmail.com>
2022-11-16 21:40:50 +0600

    Workshop Details API writting done

--- routes/workshopRoutes.js
+++ routes/workshopRoutes.js
@@ -10,23 +10,40 @@ router.get(
     '/:id',
     asyncHandler(async (req, res) => {
         const event_id = req.params.id;
-        var result ={};
-        
-           return db.execute(
-                `SELECT id,title,start_at,end_at FROM events where id =${event_id}`
-            )
+        var result = {};
+
+        return db.execute(
+            `SELECT id,title,start_at,end_at FROM events where id =${event_id}`
+        )
             .then((events) => {
-                if(arrayIsEmpty(events[0])){
-                    res.status(404).json({code:404,msg:"No event found with this Id"});
-                }
-               result = events[0];
-               
-            return db.execute(`SELECT id,title,description,start_at,end_at FROM workshops where workshops.event_id in (${event_id})`)
-            }).then((workshop) => {
-                arrayIsEmpty(workshop[0])?  result[0].workshops = [] : result[0].workshops = workshop[0];
+                arrayIsEmpty(events[0])?res.status(404).json({ code: 404, msg: "No event found with this Id" }):result = events[0];
                 
-               res.status(200).json({code:200,msg: "ok",data:result[0]});
-                 
+                return db.execute(`SELECT id,title,description,start_at,end_at FROM workshops where workshops.event_id in (${event_id})`)
+            }).then((workshop) => {
+                arrayIsEmpty(workshop[0]) ? result[0].workshops = [] : result[0].workshops = workshop[0];
+
+                res.status(200).json({ code: 200, msg: "ok", data: result[0] });
+
+            })
+            .catch(err => {
+                res.status(500).json({ msg: 'Server Error' });
+            });
+    })
+);
+
+// @description  Fetch Workshop Detail By Id
+// @route        GET /api/workshops/detail/:id
+// @access       Public
+router.get(
+    '/detail/:id',
+    asyncHandler(async (req, res) => {
+        const workshop_id = req.params.id;
+
+        return db.execute(`SELECT id,title,description,start_at,end_at FROM workshops where id = ${workshop_id}`)
+            .then(([workshops, fieldData]) => {
+
+                arrayIsEmpty(workshops)?res.status(404).json({ code: 404, msg: "No workshops found with this Id" }) :res.status(200).json({ code: 200, msg: "ok", data: workshops[0] });
+                
             })
             .catch(err => {
                 res.status(500).json({ msg: 'Server Error' });
